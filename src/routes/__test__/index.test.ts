da6fc1934f41775af2ff7f9c67dbeba21dcb8218
mwinel <mwinel@live.com>
2021-04-30 09:57:16 +0300

    Update unit tests

--- src/routes/__test__/index.test.ts
+++ src/routes/__test__/index.test.ts
@@ -5,5 +5,5 @@ import { app } from "../../app";
 it("Returns `Hello World!` message.", async () => {
   const resp = await request(app).get("/").expect(200);
 
-  expect(resp.body.message).toEqual('Hello World!')
+  expect(resp.body.message).toEqual('Hello World!..')
 });
